81d30ad9d2023121e599e597116c9bb6e98a1568
HysteriaKa <karine2310@gmail.com>
2021-11-23 13:14:56 +0100

    add trick template -test route @slug

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,24 +3,48 @@
 namespace App\Controller;
 
 use App\Entity\Trick;
+use App\Repository\TrickRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class MainController extends AbstractController
 {
+
+    private $repoTricks;
+
+    public function __construct(TrickRepository $repoTricks)
+    {
+        $this->repository = $repoTricks;
+    }
+
     /**
      * @Route("/", name="app_home")
      */
     public function index(): Response
     {
-        $tricks = $this->getDoctrine()
-            ->getRepository(Trick::class)->findAll();
-
+        // $tricks = $this->getDoctrine()
+        //     ->getRepository(Trick::class)->findAll();
+        $tricks = $this->repository->findAll();
         return $this->render('main/homePage.html.twig', [
             'controller_name' => 'MainController',
             'current_menu' => 'home',
-            'tricks' =>$tricks
+            'tricks' => $tricks
+        ]);
+    }
+
+    /**
+     * @ParamConverter("trick", class="Trick")
+     * @route("/trick/{slug}", name="trick") 
+     */
+    public function showTrick(Trick $slug, Request $request)
+    {
+        $trick= $this->repository->findOneBy(['slug' => $slug]);
+        $slug =$trick->getSlug();
+        return $this->render('main/trick.html.twig', [
+            'trick' => $trick
         ]);
     }
 }
--- src/DataFixtures/TrickFixtures.php
+++ src/DataFixtures/TrickFixtures.php
@@ -12,7 +12,7 @@ class TrickFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $categoriesName = ['Jumps', 'Rotations', 'Grabs', 'Presses', 'Rail Tricks'];
-        $trickNames = ['Ollie', 'Nollie', 'Melon', 'Indy', 'Nose Grab', 'japan', 'Tail Press', 'Frontside 180', 'Backside 180', 'Butter', 'Air-to-fakie', 'Beef Curtains'];
+        $trickNames = ['Ollie', 'Nollie', 'Melon', 'Indy', 'Nose Grab', 'japan', 'Tail Press', 'Frontside 180', 'Backside 180', 'Butter', 'Air-to-fakie', 'Beef Curtains','truck drive','mute','sad'];
 
         foreach ($categoriesName as $categoryTag) {
             $category = new Category();
