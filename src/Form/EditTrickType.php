5facfd5ecb9df31c46dab823807f39aa7d17e3e3
HysteriaKa <karine2310@gmail.com>
2021-11-30 09:44:43 +0100

    form - controller - no category yet

--- src/Form/EditTrickType.php
+++ src/Form/EditTrickType.php
@@ -3,22 +3,25 @@
 namespace App\Form;
 
 use App\Entity\Trick;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class EditTrickType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('createdAt')
-            ->add('updateAt')
-            ->add('slug')
-            ->add('category')
-        ;
+            ->add('name', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('slug', TextType::class)
+            // ->add('category', EntityType::class,['class'=>Category::class])
+            ->add("Submit", SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
