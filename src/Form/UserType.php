b09bf1a748abaf6b28e008702d768959b71f381e
HysteriaKa <karine2310@gmail.com>
2021-12-13 16:32:12 +0100

    Edit profil controlleur template form

---
+++ src/Form/UserType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Validator\Constraints;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+
+            ->add('username')
+            ->add('avatar', FileType::class, [
+                'label' => 'Your best picture (jpeg, jpg, png)',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new Image([
+                        'maxSize' => '2M',
+                 
+                        'mimeTypesMessage' => 'Please upload a valid format image',
+                    ])
+                ]
+            ])
+            ->add("Submit", SubmitType::class, ['attr' => ['class' => 'btn-dark']]);;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
